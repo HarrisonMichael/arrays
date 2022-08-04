0f912ce4594ab44e900e9bc5677365de7b676d6d
HarrisonMichael <109280101+HarrisonMichael@users.noreply.github.com>
2022-08-04 06:16:44 +0100

    Add files via upload

---
+++ arrays/arrays.js
@@ -0,0 +1,5 @@
+let names = ["michael","gift","kachi","grant"]
+let number = [1,3,5,[[[3,4]]]]
+console.log(number.flat(3))
+console.log(number.fill("harrison"))
+console.log(names.includes("Emmanuel"))
